ca1f86f09609ed9805fbc213b7a5ea1c307f726a
Diego Sigcho Alvarado <sigchodiego@gmail.com>
2024-08-17 22:11:31 -0500

    Update AdminLTE template

--- config/global.php
+++ config/global.php
@@ -6,3 +6,8 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_ENCODE', 'utf8');
 define('SYSTEM_NAME', 'POS_SYSTEM');
+
+function asset($path) {
+    $path = "/sistema-pos/public/$path";
+    return $path;
+}
