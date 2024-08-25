020e0009fed6ae5fbb7aae5a41d8adf1b52066e0
Diego Sigcho Alvarado <sigchodiego@gmail.com>
2024-08-25 18:37:37 -0500

    Update category view

--- views/categorias.php
+++ views/categorias.php
@@ -8,8 +8,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src=<?= asset('dist/img/AdminLTELogo.png'); ?>"dist/img/AdminLTELogo.png"
-                alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src=<?= asset('dist/img/AdminLTELogo.png'); ?> alt="AdminLTELogo" height="60" width="60">
         </div>
 
         <!-- Navbar -->
